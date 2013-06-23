36205fdf256c2e6fe198ae6421b975c5c4e8e6da
Your Name <you@example.com>
2013-06-24 01:36:13 +0800

    add new examples

--- ast.cpp
+++ ast.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 using namespace llvm;
 
+const int CODEGENDEBUG = 0;
+
 stack<IRBuilder<>*> builders;
 vector<Declarations*> globaldec;
 Module *module;
@@ -79,87 +81,87 @@ Function* findfunc(Ident* id)
 
 void node::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void Ident::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void Declaration::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void typeDec::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void aliasType::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void arrayType::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void typeField::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void typeFields::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void structType::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void Declarations::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	Declarations* decblk = new Declarations;
 	globaldec.push_back(decblk);
 	for(int i = 0; i < vec.size(); i++){
 		vec[i]->gen();
 		decblk->add(vec[i]);
 	}
-	cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
 }
 
 void Expression::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void Expressions::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	for(int i = 0; i < vec.size(); i++) vec[i]->gen();
 	if(vec.size()) v = vec.back()->v;
-	cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
 }
 
 void expBlock::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	decs->gen();
 	exp->gen();
 	v = exp->v;
 	globaldec.pop_back();
-	cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
 }
 
 void funcDec::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	Type* rettype = 0;
 	if(ret) rettype = findtype(ret);
 	else rettype = Type::getVoidTy(getGlobalContext());
@@ -172,22 +174,23 @@ void funcDec::gen()
 	BasicBlock *bblock = BasicBlock::Create(getGlobalContext(), "entry", f, 0);
     IRBuilder<>* b = new IRBuilder<>(bblock);
 	pushb(b);
-	for (ait = f->arg_begin(); ait != f->arg_end(); ait++) {
+	int i;
+	for (ait = f->arg_begin(), i = 0; ait != f->arg_end(); ait++, i++) {
 		Expression* argValue = new Expression;
 		argValue->v = ait;
-		for(int i = 0; i < params->vec.size(); i++) argDec->add(new Variable(params->vec[i]->id, argValue));
+		argDec->add(new Variable(params->vec[i]->id, argValue));
 	}
 	argDec->gen();
 	exp->gen();
 	b->CreateRet(exp->v);
 	popb();
 	globaldec.pop_back();
-	cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
 }
 
 void Variable::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	exp->gen();
 	IRBuilder<>* b = topb();
 	Type* vartype;
@@ -195,12 +198,12 @@ void Variable::gen()
 	else vartype = Type::getInt32Ty(getGlobalContext());
 	v = b->CreateAlloca(vartype, 0, id->s());
 	b->CreateStore(exp->v, v);
-	cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
 }
 
 void Program::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	module = new Module("", getGlobalContext());
 	FunctionType *funcType = FunctionType::get(Type::getInt32Ty(getGlobalContext()), false);
 	mainFunc = Function::Create(funcType, GlobalValue::InternalLinkage, "main", module);
@@ -221,38 +224,38 @@ void Program::gen()
 	b = popb();
 	b->CreateRet(exp->v);
 	//b->CreateRet(ConstantInt::get(b->getInt32Ty(), 0, true));
-	cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
 }
 
 void constValue::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void nullConstant::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void intConstant::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	IRBuilder<>* b = topb();
 	v = ConstantInt::get(Type::getInt32Ty(getGlobalContext()), i, true);
-	cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
 }
 
 void stringConstant::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	IRBuilder<>* b = topb();
 	v = b->CreateGlobalStringPtr(s);
-	cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
 }
 
 void uniOperator::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	IRBuilder<>* b = topb();
 	exp->gen();
 	Value* expv = exp->v;
@@ -261,7 +264,7 @@ void uniOperator::gen()
 
 void binOperator::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	IRBuilder<>* b = topb();
 	expa->gen();
 	expb->gen();
@@ -283,22 +286,22 @@ void binOperator::gen()
 
 void leftValue::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void assignOperator::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	IRBuilder<>* b = topb();
 	lvalue->gen();
 	exp->gen();
 	v = b->CreateStore(exp->v, lvalue->lv);
-	cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
 }
 
 void ifthenBlock::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	IRBuilder<>* b = topb();
 	cond->gen();
 	Function *TheFunction = b->GetInsertBlock()->getParent();
@@ -312,12 +315,12 @@ void ifthenBlock::gen()
 	TheFunction->getBasicBlockList().push_back(MergeBB);
 	b->SetInsertPoint(MergeBB);
 	v = 0;
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void ifelseBlock::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	IRBuilder<>* b = topb();
 	cond->gen();
 	Function *TheFunction = b->GetInsertBlock()->getParent();
@@ -340,17 +343,17 @@ void ifelseBlock::gen()
 	PN->addIncoming(thenBlock->v, ThenBB);
 	PN->addIncoming(elseBlock->v, ElseBB);
 	v = PN;
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void forBlock::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void whileBlock::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	IRBuilder<>* b = topb();
 	Function *TheFunction = b->GetInsertBlock()->getParent();
 	BasicBlock *CondBB = BasicBlock::Create(getGlobalContext(), "cond", TheFunction);
@@ -369,87 +372,87 @@ void whileBlock::gen()
 	TheFunction->getBasicBlockList().push_back(MergeBB);
 	b->SetInsertPoint(MergeBB);
 	v = 0;
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void breakLoop::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void callerParam::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	IRBuilder<>* b = topb();
 	exp->gen();
 	v = exp->v;
-	cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
 }
 
 void callerParams::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	for(int i = 0; i < vec.size(); i++) vec[i]->gen();
-	cerr << "Generating code for " << typeid(*this).name() << " done" << endl;	
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << " done" << endl;	
 }
 
 void functionCaller::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	IRBuilder<>* b = topb();
 	params->gen();
 	vector<Value*> vec;
 	for(int k = 0; k < params->vec.size(); k++) vec.push_back(params->vec[k]->v);
 	v = b->CreateCall(findfunc(id), makeArrayRef(vec));
-	cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
 }
 
 void arrayOperator::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void arrayOperators::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void structField::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void structFields::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void structConstant::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void arrayLvalue::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void arrayConstant::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
 void varLvalue::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 	IRBuilder<>* b = topb();
 	lv = findvar(id);
 	v = b->CreateLoad(lv);
-	cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << " done" << endl;
 }
 
 void structLvalue::gen()
 {
-	cerr << "Generating code for " << typeid(*this).name() << endl;
+	if (CODEGENDEBUG == 1) cerr << "Generating code for " << typeid(*this).name() << endl;
 }
 
